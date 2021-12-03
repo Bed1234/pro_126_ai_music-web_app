e7fa20744f3fab365b8eb0149930ff13ff46fca0
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-12-03 18:52:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY =0;
 
+
+left_wrist_score = 0;
+right_wrist_score = 0;
+//song_status1 = "Tera Naam.mp3";
+//song_status2 = "";
+
 function preload(){
     song = loadSound("Tera Naam.mp3");
     song_2 = loadSound("Param Sundari.mp3")
@@ -24,18 +30,21 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 
-
+function modelLoaded() {
+    console.log("Pose Net Is initialized")
+}
 
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
         
-        scoreLW = results[0].pose.keypoints[9].score;
-        console.log("scoreLW = " + scoreLW );
+        left_wrist_score = results[0].pose.keypoints[9].score;
+        right_wrist_score = results[0].pose.keypoints[10].score;
+        console.log(" right_wrist_score = " + right_wrist_score + "left_wrist_score" + left_wrist_score );
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log(" leftWristX = "+leftWristX +  " leftWristY = " + leftWristY);
+        console.log(" leftWristX = "+ leftWristX +  " leftWristY = " +  leftWristY);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
@@ -46,12 +55,30 @@ function gotPoses(results) {
     } 
 }
 
-function modelLoaded() {
-    console.log("Pose Net Is initialized")
-}
 
+//var song_status1 = song;
+//var song_status2 = song_2;
 function draw(){
     image (video,0,0,600,500);
+
+    
+    fill ("#FF0000");
+    stroke ("#FF0000");
+   
+
+
+    if( left_wrist_score > 0.2){
+    circle( leftWristX , leftWristY , 20 );
+    song_2.stop();
+    song.play();
+
+    }
+
+   // if(song_status2 == false ){
+       // song.play();
+        //document.getElementById("lbl_song_name").innerHTML = "song = " + "Tera Naam";
+//  }
+
 }
 
 
